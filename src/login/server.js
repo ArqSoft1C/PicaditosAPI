b284e1186e8b0f4c0bf30aa46afc01f2adbe523a
smarquezo <smarquezo@unal.edu.co>
2018-11-29 14:19:50 -0500

    .

--- src/login/server.js
+++ src/login/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.LOGIN_URL
-export const port = process.env.LOGIN_PORT
-export const entryPoint = process.env.LOGIN_ENTRY
+export const url = process.env.LOGIN_URL || 'login_ms';
+export const port = process.env.LOGIN_PORT || '4001';
+export const entryPoint = process.env.LOGIN_ENTRY || 'auth';
